9ce079e9fe2cc9a03a2ea00f42914dfd06f5777c
Rehan <rehanalire52@gmail.com>
2024-05-28 14:29:15 +0530

    add authentication  middleware to check if user is login ot not

---
+++ middleware/auth.middleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+
+export const check = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication token is required. Please login.",
+      });
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+        console.error('Token verification failed:', error);
+      return res
+        .status(401)
+        .json({ success: false,  message: "Invalid authentication token. Please login again."});
+    }
+  } catch (error) {
+    console.error('Error during authentication check:', error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error while validating the token.",
+      error: error.message,
+    });
+  }
+};
