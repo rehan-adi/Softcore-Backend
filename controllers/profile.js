96c487ec4f180e01498d66601c003579d8c61fed
Rehan <rehanalire52@gmail.com>
2024-06-03 20:15:46 +0530

    add edit profile feature

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,32 +2,71 @@ import userModel from "../models/Blog_user.model.js";
 import postModel from "../models/post.model.js";
 
 export const getProfile = async (req, res) => {
-    try {
-        const userId = req.user.id;
+  try {
+    const userId = req.user.id;
 
-        const checkProfile = await userModel.findById(userId);
+    const checkProfile = await userModel.findById(userId);
 
-        if(!checkProfile) {
-            return res.status(404).json({
-                success: false,
-                message: "Profile not found",
-            });
-        }
-
-        const userPosts = await postModel.find({ author: userId });
-
-        return res.status(200).json({
-            success: true,
-            profile: checkProfile,
-            posts: userPosts,
-        });
-
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            success: false,
-            message: "Failed to get profile",
-            error: error.message,
-        });
+    if (!checkProfile) {
+      return res.status(404).json({
+        success: false,
+        message: "Profile not found",
+      });
     }
+
+    const userPosts = await postModel.find({ author: userId });
+
+    return res.status(200).json({
+      success: true,
+      profile: checkProfile,
+      posts: userPosts,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to get profile",
+      error: error.message,
+    });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { username, profilePicture, bio } = req.body;
+    const userId = req.user.id;
+    const profile = await userModel.findById(userId);
+
+    if (!profile) {
+      return res.status(404).json({
+        success: false,
+        message: "Profile not found",
+      });
+    }
+
+    const updatedProfileData = {
+      ...(username && { username }), // Update username if provided
+      ...(profilePicture && { profilePicture }), // Update profile picture if provided
+      ...(bio && { bio }), // Update bio if provided
+    };
+
+    const updatedProfile = await userModel.findByIdAndUpdate(
+      userId,
+      updatedProfileData,
+      { new: true }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Profile updated successfully",
+      profile: updatedProfile,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to get profile",
+      error: error.message,
+    });
+  }
 };
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getProfile } from '../controllers/profile.js'
+import { getProfile, updateProfile } from '../controllers/profile.js'
 
 import {checkLogin} from '../middleware/auth.middleware.js'
 
 const profileRouter = express.Router();
 
 profileRouter.get('/:id', checkLogin, getProfile);
+profileRouter.patch('/:id', checkLogin, updateProfile);
 
 export default profileRouter;
