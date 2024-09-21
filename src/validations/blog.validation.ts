f55c1a2e0453ba40b4eda3b540c6bc677578965d
Rehan <rehanalire52@gmail.com>
2024-09-21 18:42:08 +0530

    remove title from validation

--- src/validations/blog.validation.ts
+++ src/validations/blog.validation.ts
@@ -1,11 +1,6 @@
 import { z } from 'zod';
 
-// Define validation schema for creating a blog
 export const createBlogValidation = z.object({
-    title: z
-        .string()
-        .max(100, { message: 'Title cannot exceed 100 characters' })
-        .transform((val) => val.trim()),
     content: z
         .string()
         .min(10, { message: 'Content should have at least 10 characters' })
