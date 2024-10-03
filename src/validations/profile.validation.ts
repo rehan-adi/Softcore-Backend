55c84517413a48725834214c9f298b7f32b3aaac
Rehan <rehanalire52@gmail.com>
2024-10-03 21:14:47 +0530

    small update on profile validation

--- src/validations/profile.validation.ts
+++ src/validations/profile.validation.ts
@@ -1,7 +1,14 @@
 import { z } from 'zod';
 
 export const updateProfileValidation = z.object({
-    username: z.string().optional(),
-    bio: z.string().optional(),
-    image: z.string().optional(),
+    username: z
+        .string()
+        .min(2, 'Username must be at least 2 characters long')
+        .max(20, 'Username must be at most 20 characters long')
+        .optional(),
+    bio: z
+        .string()
+        .max(200, 'Bio must be at most 200 characters long')
+        .optional(),
+    image: z.string().optional()
 });
