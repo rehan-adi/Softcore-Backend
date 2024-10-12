47a00e4e73ca0e76d4e121e4eb1e56cebae92080
Rehan <rehanalire52@gmail.com>
2024-10-12 20:12:52 +0530

    feat: implement changePassword controller with validation, password hashing, and error handling

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,13 +1,14 @@
-import { Request, Response } from 'express';
-import userModel from '../models/user.model.js';
 import bcrypt from 'bcrypt';
+import { ZodError } from 'zod';
 import jwt from 'jsonwebtoken';
 import config from '../config/config.js';
+import { Request, Response } from 'express';
+import userModel from '../models/user.model.js';
 import {
     signinValidation,
     signupValidation
 } from '../validations/user.validation.js';
-import { ZodError } from 'zod';
+import { changePasswordValidation } from '../validations/user.validation.js';
 
 export const signup = async (req: Request, res: Response) => {
     try {
@@ -33,7 +34,7 @@ export const signup = async (req: Request, res: Response) => {
             fullname,
             email,
             password: hashedPassword,
-            profilePicture,
+            profilePicture
         });
 
         // Return the created user details (excluding the password)
@@ -151,6 +152,41 @@ export const logout = (req: Request, res: Response) => {
         return res.status(500).json({
             success: false,
             message: 'Failed to log out',
+            error: error instanceof Error ? error.message : 'Unknown error'
+        });
+    }
+};
+
+export const changePassword = async (req: Request, res: Response) => {
+    try {
+        const userId = req.user?.id;
+        const parsedData = changePasswordValidation.parse(req.body);
+
+        const { password } = parsedData;
+
+        const user = await userModel.findById(userId);
+        if (!user) {
+            return res
+                .status(404)
+                .json({ success: false, message: 'User not found' });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await userModel.findByIdAndUpdate(
+            userId,
+            { password: hashedPassword },
+            { new: true }
+        );
+
+        return res
+            .status(200)
+            .json({ success: true, message: 'Password updated successfully' });
+    } catch (error) {
+        console.error('Error during changing password:', error);
+        return res.status(500).json({
+            success: false,
+            message: 'Failed to change password',
             error: error instanceof Error ? error.message : 'Unknown error'
         });
     }
