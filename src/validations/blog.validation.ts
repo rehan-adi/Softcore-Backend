01a951f52ebb7a1fc8a7659b93096138555298eb
Rehan <rehanalire52@gmail.com>
2024-11-22 19:14:11 +0530

    add optional to content and category

--- src/validations/blog.validation.ts
+++ src/validations/blog.validation.ts
@@ -3,13 +3,11 @@ import { z } from 'zod';
 export const createPostValidation = z.object({
     content: z
         .string()
-        .min(1, { message: 'Content should have at least 1 characters' })
-        .transform((val) => val.trim()),
+        .optional(),
     tags: z.array(z.string()).optional(),
     category: z
         .string()
-        .min(1, { message: 'Category is required' })
-        .transform((val) => val.trim())
+        .optional()
 });
 
 export const updatePostValidation = z.object({
